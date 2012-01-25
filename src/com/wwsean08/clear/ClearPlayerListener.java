e54fae964fea133e9f414aa0e09b4797c9f6c8ee
Sean Smith <wwsportsguy@gmail.com>
2012-01-25 12:26:16 -0500

    updated to use the newest event listener system from Bukkit 1.1 R1

--- src/com/wwsean08/clear/ClearPlayerListener.java
+++ src/com/wwsean08/clear/ClearPlayerListener.java
@@ -2,22 +2,25 @@ package com.wwsean08.clear;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-public class ClearPlayerListener extends PlayerListener{
+public class ClearPlayerListener implements Listener{
 	Clear plugin;
 	public ClearPlayerListener(Clear instance){
 		plugin = instance;
-		Bukkit.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, this, Event.Priority.Monitor, plugin);
-		Bukkit.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_KICK, this, Event.Priority.Monitor, plugin);
+		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
-	@Override
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerQuit(PlayerQuitEvent event){
 		plugin.preview.unpreview(event.getPlayer());
 	}
-	@Override
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerKick(PlayerKickEvent event){
 		plugin.preview.unpreview(event.getPlayer());
 	}
