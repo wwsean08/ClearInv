e4e55b369b6f6ae797400757e50092db3968644e
Sean Smith <wwsportsguy@gmail.com>
2012-03-08 23:04:24 -0500

    added what I believe are all the listeners needed for the continuous preview mode

--- src/com/wwsean08/clear/ClearPlayerListener.java
+++ src/com/wwsean08/clear/ClearPlayerListener.java
@@ -4,12 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class ClearPlayerListener implements Listener{
 	Clear plugin;
-	
+
 	/**
 	 * the public constructor for the player listener
 	 * @param instance is the class that called us
@@ -18,7 +23,7 @@ public class ClearPlayerListener implements Listener{
 		plugin = instance;
 		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
-	
+
 	/**
 	 * a player quit event happened so we gotta make sure they weren't previewing an inventory
 	 * @param event
@@ -27,7 +32,7 @@ public class ClearPlayerListener implements Listener{
 	public void onPlayerQuit(PlayerQuitEvent event){
 		plugin.preview.unpreview(event.getPlayer());
 	}
-	
+
 	/**
 	 * a player got kicked so make sure they weren't previewing an inventory
 	 * @param event
@@ -35,5 +40,85 @@ public class ClearPlayerListener implements Listener{
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerKick(PlayerKickEvent event){
 		plugin.preview.unpreview(event.getPlayer());
+	}
+
+	/**
+	 * used for when we are in continuous preview mode
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onBlockPlace(BlockPlaceEvent event){
+		for(PreviewHolder a : plugin.preview.previewList){
+			if(a.getObserved().getName().equals(event.getPlayer().getName())){
+				if(a.getContinuous()){
+					a.getObserver().getInventory().clear();
+					a.getObserver().getInventory().setContents(event.getPlayer().getInventory().getContents());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * used for when we are in continuous preview mode
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onInventoryClose(InventoryCloseEvent event){
+		for(PreviewHolder a : plugin.preview.previewList){
+			if(a.getObserved().getName().equals(event.getPlayer().getName())){
+				if(a.getContinuous()){
+					a.getObserver().getInventory().clear();
+					a.getObserver().getInventory().setContents(event.getPlayer().getInventory().getContents());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * used for when we are in continuous preview mode
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerChangedWorld(PlayerChangedWorldEvent event){
+		for(PreviewHolder a : plugin.preview.previewList){
+			if(a.getObserved().getName().equals(event.getPlayer().getName())){
+				if(a.getContinuous()){
+					a.getObserver().getInventory().clear();
+					a.getObserver().getInventory().setContents(event.getPlayer().getInventory().getContents());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * used for when we are in continuous preview mode
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerPickupItem(PlayerPickupItemEvent event){
+		for(PreviewHolder a : plugin.preview.previewList){
+			if(a.getObserved().getName().equals(event.getPlayer().getName())){
+				if(a.getContinuous()){
+					a.getObserver().getInventory().clear();
+					a.getObserver().getInventory().setContents(event.getPlayer().getInventory().getContents());
+				}
+			}
+		}
+	}
+
+	/**
+	 * used for when we are in continuous preview mode
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerRespawn(PlayerRespawnEvent event){
+		for(PreviewHolder a : plugin.preview.previewList){
+			if(a.getObserved().getName().equals(event.getPlayer().getName())){
+				if(a.getContinuous()){
+					a.getObserver().getInventory().clear();
+					a.getObserver().getInventory().setContents(event.getPlayer().getInventory().getContents());
+				}
+			}
+		}
 	}
 }
